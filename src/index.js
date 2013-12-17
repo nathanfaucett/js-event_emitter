6ed516820070da101f9d90393e21e390661d02b6
lonewolfgames <nathanfaucett@gmail.com>
2013-12-17 07:23:25 -0600

    update on with passing multiple types 'event1, event2'

--- src/index.js
+++ src/index.js
@@ -1,93 +1,114 @@
-var shift = Array.prototype.shift;
+var shift = Array.prototype.shift,
+	SPLITER = /[ ,]+/;
 
 
 function EventEmitter() {
 
-    this._events = {};
+	this._events = {};
 }
 
 
-EventEmitter.prototype.on = EventEmitter.prototype.addEventListener = function(type, listener) {
+EventEmitter.prototype.on = EventEmitter.prototype.addEventListener = function(type, listener, ctx) {
+	var types = type.split(SPLITER),
+		events = this._events,
+		i;
 	
-    (this._events[type] || (this._events[type] = [])).push(listener);
-
-    return this;
+	for (i = types.length; i--;) {
+		type = types[i];
+		(events[type] || (events[type] = [])).push({listener: listener, ctx: ctx || this});
+	}
+	
+	return this;
 };
 
 
-EventEmitter.prototype.once = EventEmitter.prototype.addEventListenerOnce = function(type, listener) {
+EventEmitter.prototype.once = EventEmitter.prototype.addEventListenerOnce = function(type, listener, ctx) {
+	var self = this;
+	ctx || (ctx = this);
 	
-    function once() {
-
-        this.off(type, once);
-        listener.apply(this, arguments);
-    }
-
-    (this._events[type] || (this._events[type] = [])).push(once);
-
-    return this;
+	function once() {
+		self.off(type, once);
+		listener.apply(ctx, arguments);
+	}
+	
+	return this.on(type, once, ctx);
 };
 
 
-EventEmitter.prototype.off = EventEmitter.prototype.removeEventListener = function(type, listener) {
-    var events, handler,
-        i;
-
-    if (!type) {
-        events = this._events;
-        for (i in events) events[i].length = 0;
-        return this;
-    }
+EventEmitter.prototype.listenTo = EventEmitter.prototype.addEventListenerTo = function(obj, type, listener, ctx) {
+	if (!(obj instanceof EventEmitter)) throw "Can't listen to Object, it's not a instance of EventEmitter";
 	
-	events = this._events[type];
-    if (!events) return this;
+	obj.on(type, listener, ctx || this);
+	
+	return this;
+};
 
-    if (!listener) {
-        events.length = 0;
-    } else {
-        for (i = events.length; i--;) {
-            handler = events[i];
 
-            if (listener === handler) {
-                events.splice(i, 1);
-                return this;
-            }
-        }
-    }
+EventEmitter.prototype.off = EventEmitter.prototype.removeEventListener = function(type, listener, ctx) {
+	var types = type.split(SPLITER),
+		thisEvents = this._events, events, event,
+		i, j;
 
-    return this;
+	for (i = types.length; i--;) {
+		type = types[i];
+		
+		if (!type) {
+			for (i in thisEvents) thisEvents[i].length = 0;
+			return this;
+		}
+		
+		events = thisEvents[type];
+		if (!events) return this;
+	
+		if (!listener) {
+			events.length = 0;
+		} else {
+			ctx || (ctx = this);
+			
+			for (j = events.length; j--;) {
+				event = events[j];
+	
+				if (event.listener === listener && event.ctx === ctx) {
+					events.splice(j, 1);
+					break;
+				}
+			}
+		}
+	}
+
+	return this;
 };
 
 
 EventEmitter.prototype.emit = EventEmitter.prototype.trigger = function(type) {
-    var events = this._events[type],
+	var events = this._events[type],
 		a1, a2, a3, a4,
 		event,
-        i;
-
-    if (!events || !events.length) return this;
+		i;
+	
+	if (!events || !events.length) return this;
 	
 	switch(arguments.length){
 		case 1:
-			for (i = events.length; i--;) events[i].call(this);
+			for (i = events.length; i--;) (event = events[i]).listener.call(event.ctx);
 			break;
 		
 		case 2:
 			a1 = arguments[1];
-			for (i = events.length; i--;) events[i].call(this, a1);
+			for (i = events.length; i--;) (event = events[i]).listener.call(event.ctx, a1);
 			break;
 		
 		case 3:
 			a1 = arguments[1];
 			a2 = arguments[2];
-			for (i = events.length; i--;) events[i].call(this, a1, a2);
+			for (i = events.length; i--;) (event = events[i]).listener.call(event.ctx, a1, a2);
 			break;
 		
 		case 4:
 			a1 = arguments[1];
 			a2 = arguments[2];
 			a3 = arguments[3];
-			for (i = events.length; i--;) events[i].call(this, a1, a2, a3);
+			for (i = events.length; i--;) (event = events[i]).listener.call(event.ctx, a1, a2, a3);
 			break;
 		
 		case 5:
@@ -95,15 +116,15 @@ EventEmitter.prototype.emit = EventEmitter.prototype.trigger = function(type) {
 			a2 = arguments[2];
 			a3 = arguments[3];
 			a4 = arguments[4];
-			for (i = events.length; i--;) events[i].call(this, a1, a2, a3, a4);
+			for (i = events.length; i--;) (event = events[i]).listener.call(event.ctx, a1, a2, a3, a4);
 			break;
 		
 		default:
 			shift.apply(arguments);
-			for (i = events.length; i--;) events[i].apply(this, arguments);
+			for (i = events.length; i--;) (event = events[i]).listener.apply(event.ctx, arguments);
 	}
 
-    return this;
+	return this;
 };
 
 
