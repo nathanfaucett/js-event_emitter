e15ef8240015c7caf0297ffed815743e88a14163
lonewolfgames <nathanfaucett@gmail.com>
2014-07-12 17:56:55 -0500

    added max listeners, internal function updates

--- src/index.js
+++ src/index.js
@@ -1,140 +1,190 @@
 var shift = Array.prototype.shift,
-	SPLITER = /[ ,]+/;
+	has = Object.prototype.hasOwnProperty;
+
+
+function EventObject(listener, ctx) {
+    this.listener = listener;
+    this.ctx = ctx;
+}
 
 
 function EventEmitter() {
 
-	this._events = {};
+    this._events = {};
+    this._maxListeners = EventEmitter.defaultMaxListeners;
 }
 
+EventEmitter.defaultMaxListeners = 10;
 
-EventEmitter.prototype.on = EventEmitter.prototype.addEventListener = function(type, listener, ctx) {
-	var types = type.split(SPLITER),
-		events = this._events,
-		i;
-	
-	for (i = types.length; i--;) {
-		type = types[i];
-		(events[type] || (events[type] = [])).push({listener: listener, ctx: ctx || this});
-	}
-	
-	return this;
+EventEmitter.prototype.on = function(type, listener, ctx) {
+    var events = this._events,
+        event = (events[type] || (events[type] = [])),
+        maxListeners = this._maxListeners;
+
+    event.push(new EventObject(listener, ctx || this));
+
+    if (maxListeners !== -1 && event.length > maxListeners) {
+        console.error("EventEmitter.on(type, listener, ctx) possible EventEmitter memory leak detected. " + maxListeners + " listeners added");
+    }
+
+    return this;
 };
 
+EventEmitter.prototype.addListener = EventEmitter.prototype.on;
 
-EventEmitter.prototype.once = EventEmitter.prototype.addEventListenerOnce = function(type, listener, ctx) {
-	var self = this;
-	ctx || (ctx = this);
-	
-	function once() {
-		self.off(type, once);
-		listener.apply(ctx, arguments);
-	}
-	
-	return this.on(type, once, ctx);
+EventEmitter.prototype.once = function(type, listener, ctx) {
+    var _this = this;
+    ctx || (ctx = this);
+
+    function once() {
+        _this.off(type, once, ctx);
+        var length = arguments.length;
+
+        if (length === 0) {
+            return listener.call(ctx);
+        } else if (length === 1) {
+            return listener.call(ctx, arguments[0]);
+        } else if (length === 2) {
+            return listener.call(ctx, arguments[0], arguments[1]);
+        } else if (length === 3) {
+            return listener.call(ctx, arguments[0], arguments[1], arguments[2]);
+        } else if (length === 4) {
+            return listener.call(ctx, arguments[0], arguments[1], arguments[2], arguments[3]);
+        } else if (length === 5) {
+            return listener.call(ctx, arguments[0], arguments[1], arguments[2], arguments[3], arguments[4]);
+        }
+
+        return listener.apply(ctx, arguments);
+    }
+
+    return this.on(type, once, ctx);
 };
 
+EventEmitter.prototype.listenTo = function(obj, type, listener, ctx) {
+    if (!(has.call(obj, "on") && typeof(obj.on) === "function")) {
+		throw new TypeError("EventEmitter.listenTo(obj, type, listener, ctx) obj must extend EventEmitter");
+    }
 
-EventEmitter.prototype.listenTo = EventEmitter.prototype.addEventListenerTo = function(obj, type, listener, ctx) {
-	if (!obj.on || !obj.addEventListenerTo) throw "Can't listen to Object, it's not a instance of EventEmitter";
-	
-	obj.on(type, listener, ctx || this);
-	
-	return this;
+    obj.on(type, listener, ctx || this);
+
+    return this;
 };
 
+EventEmitter.prototype.off = function(type, listener, ctx) {
+    var thisEvents = this._events,
+        events, event,
+        i;
 
-EventEmitter.prototype.off = EventEmitter.prototype.removeEventListener = function(type, listener, ctx) {
-	var types = type.split(SPLITER),
-		thisEvents = this._events, events, event,
-		i, j;
+    if (!type) {
+        for (var key in thisEvents) {
+            if ((events = thisEvents[key])) events.length = 0;
+        }
 
-	for (i = types.length; i--;) {
-		type = types[i];
-		
-		if (!type) {
-			for (i in thisEvents) thisEvents[i].length = 0;
-			return this;
-		}
-		
-		events = thisEvents[type];
-		if (!events) return this;
-	
-		if (!listener) {
-			events.length = 0;
-		} else {
-			ctx || (ctx = this);
-			
-			for (j = events.length; j--;) {
-				event = events[j];
-	
-				if (event.listener === listener && event.ctx === ctx) {
-					events.splice(j, 1);
-					break;
-				}
-			}
-		}
-	}
+        return this;
+    }
 
-	return this;
+    events = thisEvents[type];
+    if (!events) return this;
+
+    if (!listener) {
+        events.length = 0;
+    } else {
+        ctx = ctx || this;
+        i = events.length;
+
+        while (i--) {
+            event = events[i];
+
+            if (event.listener === listener && event.ctx === ctx) {
+                events.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+    return this;
 };
 
+EventEmitter.prototype.removeListener = EventEmitter.prototype.off;
 
-EventEmitter.prototype.emit = EventEmitter.prototype.trigger = function(type) {
-	var events = this._events[type],
-		a1, a2, a3, a4,
-		event,
-		i;
-	
-	if (!events || !events.length) return this;
-	
-	switch(arguments.length){
-		case 1:
-			for (i = events.length; i--;) (event = events[i]).listener.call(event.ctx);
-			break;
-		
-		case 2:
-			a1 = arguments[1];
-			for (i = events.length; i--;) (event = events[i]).listener.call(event.ctx, a1);
-			break;
-		
-		case 3:
-			a1 = arguments[1];
-			a2 = arguments[2];
-			for (i = events.length; i--;) (event = events[i]).listener.call(event.ctx, a1, a2);
-			break;
-		
-		case 4:
-			a1 = arguments[1];
-			a2 = arguments[2];
-			a3 = arguments[3];
-			for (i = events.length; i--;) (event = events[i]).listener.call(event.ctx, a1, a2, a3);
-			break;
-		
-		case 5:
-			a1 = arguments[1];
-			a2 = arguments[2];
-			a3 = arguments[3];
-			a4 = arguments[4];
-			for (i = events.length; i--;) (event = events[i]).listener.call(event.ctx, a1, a2, a3, a4);
-			break;
-		
-		default:
-			shift.apply(arguments);
-			for (i = events.length; i--;) (event = events[i]).listener.apply(event.ctx, arguments);
-	}
+EventEmitter.prototype.removeAllListeners = function() {
+    var events = this._events,
+        event;
 
-	return this;
+    for (var key in events) {
+        if ((event = events[i])) event.length = 0;
+    }
 };
 
+EventEmitter.prototype.emit = function(type) {
+    var events = this._events[type],
+        a1, a2, a3, a4,
+        length, event,
+        i;
+
+    if (!events || !events.length) return this;
+    length = arguments.length;
+
+    if (length === 1) {
+        i = events.length;
+        while (i--) {
+            (event = events[i]).listener.call(event.ctx);
+        }
+    } else if (length === 2) {
+        a1 = arguments[1];
+        i = events.length;
+        while (i--) {
+            (event = events[i]).listener.call(event.ctx, a1);
+        }
+    } else if (length === 3) {
+        a1 = arguments[1];
+        a2 = arguments[2];
+        i = events.length;
+        while (i--) {
+            (event = events[i]).listener.call(event.ctx, a1, a2);
+        }
+    } else if (length === 4) {
+        a1 = arguments[1];
+        a2 = arguments[2];
+        a3 = arguments[3];
+        i = events.length;
+        while (i--) {
+            (event = events[i]).listener.call(event.ctx, a1, a2, a3);
+        }
+    } else if (length === 5) {
+        a1 = arguments[1];
+        a2 = arguments[2];
+        a3 = arguments[3];
+        a4 = arguments[4];
+        i = events.length;
+        while (i--) {
+            (event = events[i]).listener.call(event.ctx, a1, a2, a3, a4);
+        }
+    } else {
+        shift.apply(arguments);
+        i = events.length;
+        while (i--) {
+            (event = events[i]).listener.apply(event.ctx, arguments);
+        }
+    }
+
+    return this;
+};
+
+EventEmitter.prototype.setMaxListeners = function(count) {
+    this._maxListeners = count;
+    return this;
+};
 
 EventEmitter.extend = function(child, parent) {
-	if (!parent) parent = this;
+    if (!parent) parent = this;
 
-	child.prototype = Object.create(parent.prototype);
-	child.prototype.constructor = child;
+    child.prototype = Object.create(parent.prototype);
+    child.prototype.constructor = child;
+    child.prototype._super = parent.prototype;
+    child.extend = parent.extend;
 
-	child.extend = this.extend;
+    return child;
 };
 
 
